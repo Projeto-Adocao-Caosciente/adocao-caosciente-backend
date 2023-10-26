1651364098e94151c7fed270734b7f98b5bb8244
Ryan S <ryanssteles@gmail.com>
2023-10-26 10:09:46 -0300

    CT-95 refactor: tratando erros na ong_service

--- app/services/ong_service.py
+++ app/services/ong_service.py
@@ -1,6 +1,8 @@
+import bycrypt
 from app.domain.models.ong import OngModel
 from app.domain.database.db import Database
 from bson import ObjectId
+from pymongo.results import InsertOneResult
 from app.services.animal_service import AnimalService
 
 class OngService:
@@ -8,60 +10,87 @@ class OngService:
         self.db = Database()
         self.ongs_collection = self.db.get_database().get_collection("ongs")
     
-    def create_ong(self, ong: OngModel) -> int:
-        with self.db.session.start_transaction():
-            result = self.ongs_collection.insert_one(ong.dict())
-            return result.inserted_id
+
+    def create_ong(self, ong: OngModel) -> bool:
+        try:
+            with self.db.session.start_transaction():
+                salt = bycrypt.gensalt() # definir rounds torna a operação mais lenta
+                ong.password = bycrypt.hashpw(ong.password.encode(), salt)
+                result = self.ongs_collection.insert_one(ong.model_dump())                
+                return True if isinstance(result, InsertOneResult) else False
+        except Exception as e:
+            print(f"Error creating ong: {e}")
+            return False
     
-    # TODO: tratar erros
-    def update_ong(self, ong: OngModel, ong_id: str) -> None:
-        with self.db.session.start_transaction():
-            self.ongs_collection.update_one(
-                {"_id": ObjectId(ong_id)},
-                {"$set": ong.dict()}
-            )
+    def update_ong(self, ong: OngModel, ong_id: str) -> bool:
+        try:
+            with self.db.session.start_transaction():
+                self.ongs_collection.update_one(
+                    {"_id": ObjectId(ong_id)},
+                    {"$set": ong.model_dump()}
+                )
+                return True
+        except Exception as e:
+            print(f"Error updating ong: {e}")
+            return False
     
-    # TODO: Tratar erros
-    def delete_ong(self, ong_id: str) -> None:
-        # TODO: Deleção lógica
-        with self.db.session.start_transaction():
-            self.ongs_collection.delete_one({"_id": ong_id})
-    
+    def delete_ong(self, ong_id: str) -> bool:
+        try:
+            with self.db.session.start_transaction():
+                # TODO: Deleção lógica
+                result = self.ongs_collection.delete_one({"_id": ObjectId(ong_id)})
+                return True if result.deleted_count > 0 else False
+        except Exception as e:
+            print(f"Error deleting ong: {e}")
+            return False
+
     def get_ong(self, ong_id: str):
-        result = self.ongs_collection.find_one({"_id": ObjectId(ong_id)})
-        if result:
-            return OngService.ong_helper(result)
-        return None
+        try:
+            result = self.ongs_collection.find_one({"_id": ObjectId(ong_id)})
+            if result:
+                return OngService.ong_helper(result)
+            return None
+        except Exception as e:
+            print(f"Error getting ong: {e}")
+            return None
     
     def get_ong_by_cnpj(self, cnpj: str):
-        result = self.ongs_collection.find_one({"cnpj": cnpj})
-        if result:
-            return {"email": result["email"], "password": result["password"]}
-        return None
+        try:
+            result = self.ongs_collection.find_one({"cnpj": cnpj})
+            if result:
+                return {"email": result["email"], "password": result["password"]}
+            return None
+        except Exception as e:
+            print(f"Error getting ong by cnpj: {e}")
+            return None
 
     def get_ong_animals(self, ong_id: str) -> list:
-        result = list(self.ongs_collection.aggregate([
-            {
-                "$match": {"_id": ObjectId(ong_id)}
-            },
-            {
-                "$lookup": {
-                    "from": "animals",
-                    "localField": "animals",
-                    "foreignField": "_id",
-                    "as": "animals"
+        try:
+            result = list(self.ongs_collection.aggregate([
+                {
+                    "$match": {"_id": ObjectId(ong_id)}
+                },
+                {
+                    "$lookup": {
+                        "from": "animals",
+                        "localField": "animals",
+                        "foreignField": "_id",
+                        "as": "animals"
+                    }
+                },
+                {
+                    "$project": {
+                        "animals": 1
+                    }
                 }
-            },
-            {
-                "$project": {
-                    "animals": 1
-                }
-            }
-        ]))
-        if result:
-            animals = result[0]["animals"]
-            return [AnimalService.animal_helper(animal) for animal in animals]
-        return []
+            ]))
+            if result:
+                animals = result[0]["animals"]
+                return [AnimalService.animal_helper(animal) for animal in animals]
+            return []
+        except Exception as e:
+            print(f"Error getting ong animals: {e}")
+            return []
 
     @staticmethod
     def ong_helper(ong) -> dict:
