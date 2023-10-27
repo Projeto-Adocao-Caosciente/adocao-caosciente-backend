ab0ffdb74bc2a2c0322765abd50231f9831aef00
Ryan S <ryanssteles@gmail.com>
2023-10-26 21:48:17 -0300

    refactor: arrumando mensagem de erro do login 2

--- app/services/login_service.py
+++ app/services/login_service.py
@@ -8,7 +8,7 @@ class LoginService:
     def authenticate(self, user: str, password: str):
         ong = self.ong_service.get_ong_by_cnpj(user)
         if ong is None:
-            raise HTTPException(status_code=204, detail="invalid user or password")
+            raise HTTPException(status_code=401, detail="invalid user or password")
         if ong["password"] != password:
             raise HTTPException(status_code=401, detail="invalid user or password")
         return ong
