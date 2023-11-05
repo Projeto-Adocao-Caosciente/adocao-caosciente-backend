b6b97f118b1b0fe984457099f401275f723af733
Murilo Franchi <48134459+mrlFranchi@users.noreply.github.com>
2023-11-05 13:29:24 -0300

    Update app/utils/utils.py
    Co-authored-by: Ryan Souza <76923948+Ryrden@users.noreply.github.com>

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -2,6 +2,6 @@ class Utils:
     def treat_phone(phone_str: str) -> str:
         new_phone = ""
         for i in phone_str:
-            if i >="0" and i <="9":
+            if "0" <= i <= "9":
                 new_phone += i
         return new_phone
