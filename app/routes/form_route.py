836fc82b3afc7a2f5c42171918242ebfdbb41aff
mRlFranchi <murilo.franchi@usp.br>
2023-11-30 19:03:53 -0300

    fix refactors

--- app/routes/form_route.py
+++ app/routes/form_route.py
@@ -1,6 +1,6 @@
 from app.services.ong_service import OngService
 from app.services.animal_service import AnimalService
-from fastapi import APIRouter, Body, Depends
+from fastapi import APIRouter, Body, Depends, Request
 from fastapi.responses import JSONResponse
 from app.domain.models.form import FormModel
 from app.services.jwt_service import JWTBearer
@@ -18,7 +18,7 @@ jwt_bearer = JWTBearer()
 
 @router.post("/{animal_id}", dependencies=[Depends(jwt_bearer)], status_code=201)
 async def create_form(animal_id, form: FormModel = Body(..., example=FormModel.Config.json_schema_extra)):
-    ong_id = jwt_bearer.get_ong_user_id()
+    ong_id = jwt_bearer.get_user_id()
     response = form_service.create_form(ong_id, animal_id, form)
     return JSONResponse(
         status_code = response.status,
@@ -27,8 +27,8 @@ async def create_form(animal_id, form: FormModel = Body(..., example=FormModel.C
 
 # TODO: ler Form via ID, verificando se a ong tem permissão
 @router.get("/{animal_id}/{form_id}", dependencies=[Depends(jwt_bearer)], status_code=200)
-async def get_form(animal_id:str, form_id: str):
-    ong_id = jwt_bearer.get_ong_user_id()
+async def read_form_by_id(animal_id:str, form_id: str):
+    ong_id = jwt_bearer.get_user_id()
     response = form_service.get_form_by_id(ong_id, animal_id, form_id)
     return JSONResponse(
         status_code = response.status,
@@ -37,14 +37,10 @@ async def get_form(animal_id:str, form_id: str):
     
 @router.get("/questions/{animal_id}/{form_id}", dependencies=[Depends(jwt_bearer)], status_code=200)
 async def get_questions(animal_id:str, form_id: str):
-    ong_id = jwt_bearer.get_ong_user_id()
+    ong_id = jwt_bearer.get_user_id()
     response = form_service.get_questions(ong_id, animal_id, form_id)
     return JSONResponse(
         status_code = response.status,
         content=response.dict()
     )
-    
-
-
-
 
