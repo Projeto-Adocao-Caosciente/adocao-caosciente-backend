35c0f828a229df9022f1a3ec2916d8b53e4168a0
Murilo Franchi <48134459+mrlFranchi@users.noreply.github.com>
2023-12-01 19:25:35 -0300

    Update app/services/form_service.py
    Co-authored-by: Ryan Souza <76923948+Ryrden@users.noreply.github.com>

--- app/services/form_service.py
+++ app/services/form_service.py
@@ -83,8 +83,7 @@ class FormService:
             return ResponseDTO(None, message, http.HTTPStatus.BAD_REQUEST)
     
     def get_form (self, form_id: str, request_id: str = "")-> ResponseDTO:
-        if request_id == "":
-            self.logger.info(f"id = {request_id} starting get form")
+        self.logger.info(f"id = {request_id} starting get form")
         try:
             result = self.form_collection.find_one({"_id": ObjectId(form_id)})
             if result:
